c4804a921dd9b4879958f46421dccaae0b4d34e7
avile <avile@DESKTOP-DF6P6R1>
2020-07-03 18:14:00 -0500

    Archivos Base

--- src/ClasesGlobales/KakashiZombies.java
+++ src/ClasesGlobales/KakashiZombies.java
@@ -5,10 +5,30 @@
  */
 package ClasesGlobales;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
 /**
  *
  * @author avile
  */
-public class KakashiZombies {
+public class KakashiZombies extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        Parent root = FXMLLoader.load(getClass().getResource("/Designs/Menu.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
     
 }
